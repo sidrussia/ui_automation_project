452159514e4dc444a91566bc28a43fe13c589d4c
sidrussia <stalitsyn@gmail.com>
2022-11-13 11:53:00 +0300

    realize LoginPage aka test design

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 
+
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=7):
     	self.browser = browser
     	self.url = url
     	self.browser.implicitly_wait(timeout)
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage 
 import pytest
 from selenium import webdriver
 # from selenium.webdriver.common.by import By
@@ -19,3 +20,21 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+    
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+    
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+    
+def test_guest_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page. should_be_login_url()
