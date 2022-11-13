bc7f741c9546be1f50e6c9f613d28268291c008d
sidrussia <stalitsyn@gmail.com>
2022-11-13 12:28:04 +0300

    page transitions

--- test_main_page.py
+++ test_main_page.py
@@ -11,9 +11,12 @@ from selenium import webdriver
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
-    page.open()                                   # открываем страницу
-    page.go_to_login_page()              # выполняем метод страницы - переходим на страницу логина
+    page = MainPage(browser, link)                         # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+    page.open()                                                        # открываем страницу
+    page.go_to_login_page()                         
+    login_page = LoginPage(browser, browser.current_url)      # выполняем метод страницы - переходим на страницу логина
+    login_page.should_be_login_page()
+    
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
